6f5ac4726208d0aecd5608612207b67d9c486ccb
Nikolay Maksimenko <horrendos@mail.ru>
2021-11-13 21:34:08 +0300

    edit files

--- Regelar_exp.py
+++ Regelar_exp.py
@@ -1,6 +1,5 @@
 from pprint import pprint
 import re
-# читаем адресную книгу в формате CSV в список contacts_list
 import csv
 with open("phonebook_raw.csv") as f:
   rows = csv.reader(f, delimiter=",")
@@ -10,14 +9,16 @@ pattern_for_phone = r"(\+7|8)\s?\(?(\d{3})\)?\s?-?(\d{3})-?(\d{2})-?(\d{2})\s?\(
 substitution_phone = r"+7(\2)\3-\4-\5 \6\7"
 
 def fix_contact_list(contacts_list):
-  new_contacts_list = []
-  for contact in contacts_list:
-    full_name = ' '.join(contact[:3]).split(' ')
-    new_contact = [full_name[0], full_name[1], full_name[2], contact[3], contact[4], re.sub(pattern_for_phone, substitution_phone, contact[5]), contact[6]]
-    new_contacts_list.append(new_contact)
-  return new_contacts_list
+    """Распределение имени/фамилии/отчества по столбцам и приведение номера телефона к требуемому формату"""
+    new_contacts_list = []
+    for contact in contacts_list:
+        full_name = ' '.join(contact[:3]).split(' ')
+        new_contact = [full_name[0], full_name[1], full_name[2], contact[3], contact[4], re.sub(pattern_for_phone, substitution_phone, contact[5]), contact[6]]
+        new_contacts_list.append(new_contact)
+    return new_contacts_list
 
 def delete_duplicate(contacts_list):
+    """Удаление дублирующих записей"""
     for contact in contacts_list:
         first_name = contact[0]
         last_name = contact[1]
@@ -35,18 +36,14 @@ def delete_duplicate(contacts_list):
                     contact[5] = new_contact[5]
                 if contact[6] == "":
                     contact[6] = new_contact[6]
-    result_list = list()
-    for i in contacts_list:
-        if i not in result_list:
-            result_list.append(i)
+    result_list = []
+    for contact in contacts_list:
+        if contact not in result_list:
+            result_list.append(contact)
     return result_list
 
-x = fix_contact_list(contacts_list)
-pprint(delete_duplicate(x))
-
-
-# код для записи файла в формате CSV
-with open("phonebook.csv", "w") as f:
-  datawriter = csv.writer(f, delimiter=',')
-  # Вместо contacts_list подставьте свой список
-  datawriter.writerows(delete_duplicate(x))
+if __name__ == '__main__':
+    result = fix_contact_list(contacts_list)
+    with open("phonebook.csv", "w") as f:
+      datawriter = csv.writer(f, delimiter=',')
+      datawriter.writerows(delete_duplicate(result))
